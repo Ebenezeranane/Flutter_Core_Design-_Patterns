61502124142344fca00bf6f6c93f56221c566e35
= <=>
2022-10-12 14:27:41 -0400

    first commit

---
+++ core_design_patterns_flutter/bin/core_design_patterns_flutter.dart
@@ -0,0 +1,5 @@
+import 'package:core_design_patterns_flutter/core_design_patterns_flutter.dart' as core_design_patterns_flutter;
+
+void main(List<String> arguments) {
+  print('Hello world: ${core_design_patterns_flutter.calculate()}!');
+}
---
+++ core_design_patterns_flutter/test/core_design_patterns_flutter_test.dart
@@ -0,0 +1,8 @@
+import 'package:core_design_patterns_flutter/core_design_patterns_flutter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
